ef580b82b923dfc77a1c520f966044d014ae0d3a
ruchlejs <sacha.ruchlejmer@gmail.com>
2024-09-27 14:07:22 +0200

    Add migration properties

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 from .config import Config
 
 bcrypt= Bcrypt()
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app,db)
 
     from .routes import init_routes
     init_routes(app)
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,2 @@
-from .task import Task
 from .user import User
+from .task import Task
